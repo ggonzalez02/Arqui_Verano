b90fb28aa8530b86eff362e662e4132759f06b35
Maricruz Campos <maricruzcv1803@gmail.com>
2024-12-14 22:47:10 -0600

    Create prueba.asm
    Prueba para mover la pelota

---
+++ Proyecto1/prueba.asm
@@ -0,0 +1,727 @@
+bits 64
+default rel
+
+
+; Here comes the defines
+sys_read: equ 0
+sys_write:	equ 1
+sys_nanosleep:	equ 35
+sys_time:	equ 201
+sys_fcntl:	equ 72
+
+
+STDIN_FILENO: equ 0
+
+F_SETFL:	equ 0x0004
+O_NONBLOCK: equ 0x0004
+
+;screen clean definition
+row_cells:	equ 32	; set to any (reasonable) value you wish
+column_cells: 	equ 54 ; set to any (reasonable) value you wish
+column_cells2: 	equ 20 ; Para el marcador de vidas, puntaje y nivel
+array_length:	equ row_cells * column_cells + row_cells ; cells are mapped to bytes in the array and a new line char ends each row
+
+;This is regarding the sleep time
+timespec:
+    tv_sec  dq 0
+    tv_nsec dq 2000000
+
+
+;This is for cleaning up the screen
+clear:		db 27, "[2J", 27, "[H"
+clear_length:	equ $-clear
+
+
+
+; Start Message
+msg1: db "        TECNOLOGICO DE COSTA RICA        ", 0xA, 0xD
+msg2: db "        MARICRUZ CAMPOS      GABRIEL GONZALEZ        ", 0xA, 0xD
+msg3: db "        PROFESOR: ERNESTO RIVERA ALVARADO        ", 0xA, 0xD
+msg4: db "        ARQUITECTURA DE COMPUTADORAS        ", 0xA, 0xD
+msg5: db "        PROYECTO 1: ARKANOID         ", 0xA, 0xD
+msg6: db "        INSTRUCCIONES: PRESIONE A PARA MOVERSE A LA IZQUIERDA Y D PARA MOVERSE A LA DERECHA   ", 0xA, 0xD
+msg7: db "        PRESIONE ENTER PARA INICIAR        ", 0xA, 0xD
+msg8: db "        PRESIONE Q PARA SALIR        ", 0xA, 0xD
+msg1_length:	equ $-msg1
+msg2_length:	equ $-msg2
+msg3_length:	equ $-msg3
+msg4_length:	equ $-msg4
+msg5_length:	equ $-msg5
+msg6_length:	equ $-msg6
+msg7_length:	equ $-msg7
+msg8_length:	equ $-msg8
+
+; Usefull macros
+
+
+
+%macro setnonblocking 0
+	mov rax, sys_fcntl
+    mov rdi, STDIN_FILENO
+    mov rsi, F_SETFL
+    mov rdx, O_NONBLOCK
+    syscall
+%endmacro
+
+%macro unsetnonblocking 0
+	mov rax, sys_fcntl
+    mov rdi, STDIN_FILENO
+    mov rsi, F_SETFL
+    mov rdx, 0
+    syscall
+%endmacro
+
+%macro full_line 0
+    times column_cells db "X"
+	db " "                    ; Espacio entre el cuadro de juego y de puntaje
+	times column_cells2 db "X"
+    db 0x0a, 0xD
+%endmacro
+
+%macro hollow_line 0
+    db "X"
+    times column_cells-2 db " "
+    db "X"
+	db " "
+	db "X"
+	times column_cells2-2 db " "
+	db "X", 0x0a, 0xD
+%endmacro
+
+%macro print 2
+	mov rax, sys_write
+	mov rdi, 1 	; stdout
+	mov rsi, %1
+	mov rdx, %2
+	syscall
+%endmacro
+
+%macro getchar 0
+	mov     rax, sys_read
+    mov     rdi, STDIN_FILENO
+    mov     rsi, input_char
+    mov     rdx, 1 ; number of bytes
+    syscall         ;read text input from keyboard
+%endmacro
+
+%macro sleeptime 0
+	mov rax, sys_nanosleep
+	mov rdi, timespec
+	xor rsi, rsi		; ignore remaining time in case of call interruption
+	syscall			; sleep for tv_sec seconds + tv_nsec nanoseconds
+%endmacro
+
+
+
+global _start
+
+section .bss
+
+input_char: resb 1
+
+section .data
+
+	board:
+		full_line
+        %rep 30
+        hollow_line
+        %endrep
+        full_line
+	board_size:   equ   $ - board
+
+	; Added for the terminal issue
+	termios:        times 36 db 0
+	stdin:          equ 0
+	ICANON:         equ 1<<1
+	ECHO:           equ 1<<3
+	VTIME: 			equ 5
+	VMIN:			equ 6
+	CC_C:			equ 18
+
+section .text
+;;;;;;;;;;;;;;;;;;;;for the working of the terminal;;;;;;;;;;;;;;;;;
+canonical_off:
+        call read_stdin_termios
+
+        ; clear canonical bit in local mode flags
+        push rax
+        mov rax, ICANON
+        not rax
+        and [termios+12], rax
+		mov byte[termios+CC_C+VTIME], 0
+		mov byte[termios+CC_C+VMIN], 0
+        pop rax
+
+        call write_stdin_termios
+        ret
+
+echo_off:
+        call read_stdin_termios
+
+        ; clear echo bit in local mode flags
+        push rax
+        mov rax, ECHO
+        not rax
+        and [termios+12], rax
+        pop rax
+
+        call write_stdin_termios
+        ret
+
+canonical_on:
+        call read_stdin_termios
+
+        ; set canonical bit in local mode flags
+        or dword [termios+12], ICANON
+		mov byte[termios+CC_C+VTIME], 0
+		mov byte[termios+CC_C+VMIN], 1
+        call write_stdin_termios
+        ret
+
+echo_on:
+        call read_stdin_termios
+
+        ; set echo bit in local mode flags
+        or dword [termios+12], ECHO
+
+        call write_stdin_termios
+        ret
+
+read_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov rax, 36h
+        mov rbx, stdin
+        mov rcx, 5401h
+        mov rdx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+
+write_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov rax, 36h
+        mov rbx, stdin
+        mov rcx, 5402h
+        mov rdx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+
+;;;;;;;;;;;;;;;;;;;;end for the working of the terminal;;;;;;;;;;;;
+
+char_equal: equ 61
+char_space: equ 32
+char_O: equ 79
+left_direction: equ -1
+right_direction: equ 1
+
+
+section .data
+	pallet_position dq board -23 + 28 * (column_cells+column_cells2 +2)
+	pallet_size dq 3
+
+	ball_pos_x dq 20          ; Posición X inicial
+    ball_pos_y dq 15          ; Posición Y inicial
+    ball_dir_x dq 1           ; Dirección X
+    ball_dir_y dq -1          ; Dirección Y
+    last_ball_pos_x dq 20     ; Última posición X
+    last_ball_pos_y dq 15     ; Última posición Y
+    ball_speed dq 10          ; Control de velocidad
+    ball_speed_count dq 0     ; Contador de velocidad
+
+	; Bloques 
+	block1: db 'OOOO', 0
+	block2: db 'UUUU', 0
+
+	; Textos para el marcador
+    score_text db "PUNTAJE: "
+    score_length equ $ - score_text
+    level_text db "NIVEL: "
+    level_length equ $ - level_text
+    lives_text db "VIDAS: "
+    lives_length equ $ - lives_text
+
+    current_score dq 0
+    current_level dq 1
+    current_lives dq 5
+
+
+section .text
+
+;	Function: print_ball
+; This function displays the position of the ball
+; Arguments: none
+;
+; Return:
+;	Void
+
+; Función para obtener el carácter en una posición
+get_char_at_position:
+    ; Input: rax = posición Y, rbx = posición X
+    ; Output: al = carácter en la posición
+    push rdx
+    push rdi
+    
+    mov rdi, column_cells+2
+    mul rdi                    ; rax = y * (column_cells+2)
+    add rax, rbx              ; rax += x
+    add rax, board            ; rax += dirección base del tablero
+    mov al, byte [rax]        ; al = carácter en la posición
+    
+    pop rdi
+    pop rdx
+    ret
+
+; Función para limpiar la posición anterior de la pelota
+clear_ball:
+    push rax
+    push rbx
+    push rcx
+    
+    ; Obtener el carácter en la posición actual
+    mov rax, [last_ball_pos_y]
+    mov rbx, [last_ball_pos_x]
+    call get_char_at_position
+    
+    ; No limpiar si hay una X
+    cmp al, 'X'
+    je .skip_clear
+    
+    ; Calcular posición en el tablero
+    mov rax, [last_ball_pos_y]
+    mov rbx, column_cells+2
+    mul rbx
+    add rax, [last_ball_pos_x]
+    add rax, board
+    
+    ; Limpiar solo si no es una X
+    mov byte [rax], ' '
+    
+.skip_clear:
+    pop rcx
+    pop rbx
+    pop rax
+    ret
+
+; Función para mover la pelota
+move_ball:
+    push rax
+    push rbx
+    push rcx
+    
+    ; Control de velocidad
+    inc qword [ball_speed_count]
+    mov rax, [ball_speed_count]
+    cmp rax, [ball_speed]
+    jl .end
+    mov qword [ball_speed_count], 0
+    
+    ; Guardar posición actual
+    mov rax, [ball_pos_x]
+    mov [last_ball_pos_x], rax
+    mov rax, [ball_pos_y]
+    mov [last_ball_pos_y], rax
+    
+    ; Verificar próxima posición X
+    mov rax, [ball_pos_x]
+    add rax, [ball_dir_x]
+    cmp rax, 2                ; Límite izquierdo
+    jle .bounce_x
+    cmp rax, column_cells-3   ; Límite derecho
+    jge .bounce_x
+    
+    ; Mover en X si es seguro
+    mov [ball_pos_x], rax
+    jmp .check_y
+    
+.bounce_x:
+    neg qword [ball_dir_x]
+    
+.check_y:
+    ; Verificar próxima posición Y
+    mov rax, [ball_pos_y]
+    add rax, [ball_dir_y]
+    cmp rax, 2                ; Límite superior
+    jle .bounce_y
+    cmp rax, row_cells-4      ; Límite inferior
+    jge .check_paddle
+    
+    ; Verificar si la próxima posición tiene una X
+    push rax
+    mov rbx, [ball_pos_x]
+    call get_char_at_position
+    cmp al, 'X'
+    pop rax
+    je .bounce_y
+    
+    ; Mover en Y si es seguro
+    mov [ball_pos_y], rax
+    jmp .end
+    
+.bounce_y:
+    neg qword [ball_dir_y]
+    jmp .end
+    
+.check_paddle:
+    ; Verificar si golpea la paleta
+    mov rbx, [pallet_position]
+    sub rbx, board
+    xor rdx, rdx
+    mov rax, rbx
+    mov rcx, column_cells+2
+    div rcx
+    mov rbx, rdx              ; rbx = posición X de la paleta
+    
+    mov rax, [ball_pos_x]
+    add rbx, [pallet_size]
+    cmp rax, rbx
+    jg .reset_ball
+    sub rbx, [pallet_size]
+    cmp rax, rbx
+    jl .reset_ball
+    
+    neg qword [ball_dir_y]
+    jmp .end
+    
+.reset_ball:
+    mov rax, [ball_pos_y]
+    cmp rax, row_cells-3
+    jl .end
+    
+    mov qword [ball_pos_x], 20
+    mov qword [ball_pos_y], 15
+    mov qword [ball_dir_y], -1
+    dec qword [current_lives]
+    
+.end:
+    pop rcx
+    pop rbx
+    pop rax
+    ret
+
+; Función para dibujar la pelota
+print_ball:
+    push rax
+    push rbx
+    push rcx
+    
+    ; Limpiar posición anterior
+    call clear_ball
+    
+    ; Verificar si la nueva posición es válida
+    mov rax, [ball_pos_y]
+    mov rbx, [ball_pos_x]
+    call get_char_at_position
+    cmp al, 'X'               ; No dibujar si hay una X
+    je .skip_draw
+    
+    ; Calcular posición en el tablero
+    mov rax, [ball_pos_y]
+    mov rbx, column_cells+2
+    mul rbx
+    add rax, [ball_pos_x]
+    add rax, board
+    
+    ; Dibujar la pelota
+    mov byte [rax], 'O'
+    
+.skip_draw:
+    pop rcx
+    pop rbx
+    pop rax
+    ret
+
+
+;	Function: print_pallet
+; This function moves the pallet in the game
+; Arguments: none
+;
+; Return;
+;	void
+print_pallet:
+	mov r8, [pallet_position]
+	mov rcx, [pallet_size]
+	.write_pallet:
+		mov byte [r8], char_equal
+		inc r8
+		dec rcx
+		jnz .write_pallet
+
+	ret
+
+;	Function: move_pallet
+; This function is in charge of moving the pallet in a given direction
+; Arguments:
+;	rdi: left direction or right direction
+;
+; Return:
+;	void
+move_pallet:
+	cmp rdi, left_direction
+	jne .move_right
+	.move_left:
+		mov r8, [pallet_position]  ; Posicion de la paleta
+		cmp byte[r8-1], 'X'        ; Comprobar si hay una X que indique el limite del area de juego
+		je .end                    ; Si hay una X no permite moverse mas
+		mov r9, [pallet_size]	   ; Tamano de la paleta
+		mov byte [r8 + r9 - 1], char_space ; Coloca un espacio en la ultima posicion
+		dec r8  				   ; Mueve la paleta un espacio hacia la izquierda
+		mov [pallet_position], r8  ; Guarda la nueva posicion
+		jmp .end
+	.move_right:
+		mov r8, [pallet_position]
+		mov r9, [pallet_size]
+		cmp byte [r8+r9], 'X'      ; Comprobar si hay una X que indique el limite del area de juego
+		je .end					   ; Si hay una X no permite moverse mas
+		mov byte [r8], char_space
+		inc r8					   ; Mueve la paleta un espacio hacia la derecha
+		mov [pallet_position], r8
+	.end:
+	ret
+
+; Funcion: Dibujar bloques
+draw_blocks_m:
+	push rbx
+	push rdx
+	push r12
+	push r13
+
+	mov r8, board 
+	add r8, ((column_cells+3)+(column_cells2 + 3))*6  
+	sub r8, 17                         ; Iniciar 6 filas debajo del limite superior
+	mov r10, 6                        ; Filas
+	lea r12, [block1]      			  ; Guarda la direccion de memoria del bloque 1
+	lea r13, [block2]                 ; Guarda la direccion de memoria del bloque 2
+	xor rbx, rbx                      ; Para alternar entre bloque 1 y bloque 2
+
+.loop_rows:
+    cmp r10, 0
+    je .blocks_done
+
+    mov r11, 13                       ; Columnas
+    push r8                           ; Posicion inicial de la fila
+
+.loop_columns:
+    cmp r11, 0
+    je .next_row
+	test rbx, 1						  ; Si el resultado es 0 dibuja el bloque 1, si es 1 dibuja el bloque 2
+	jz .draw_block1
+
+.draw_block2:
+    mov rsi, r13              		  ; Usa bloque 2 (UUUU)
+    jmp .draw_block
+
+.draw_block1:
+    mov rsi, r12                      ; Usa bloque 1 (OOOO)
+
+.draw_block:
+    mov rcx, 4                        ; Contador para los 4 caracteres
+
+.draw_chars:
+    mov al, byte [rsi]                ; Copia un caracter del bloque
+    mov byte [r8], al				  ; Copia el caracter a la posicion del tablero
+    inc rsi							  
+    inc r8
+    dec rcx
+    jnz .draw_chars					  ; Repite hasta que se dibujen todos los caracteres
+
+    not rbx                  		  ; Cambiar el bloque para la siguiente iteracion
+    dec r11
+    jmp .loop_columns
+
+.next_row:
+    pop r8                            ; Recupera la posicion inicial de la fila
+    add r8, column_cells+column_cells2 + 3   ; Avanza a la siguiente fila
+    dec r10
+    jmp .loop_rows
+
+.blocks_done:
+    pop r13
+    pop r12
+    pop rdx
+    pop rbx
+    ret
+
+; Funcion: Mostrar la puntuacion
+score_info:
+	push rbx
+	push rdx
+	push r12
+	push r13
+
+	mov r8, board
+	add r8, (column_cells+5)*5       
+	sub r8, 5						; Se ubica la posicion donde se quiere colocar el puntaje
+	mov rsi, score_text			    ; Texto 'PUNTAJE'
+	mov rcx, score_length			; Longitud del texto
+	call write_score				; Se llama a la funcion para escribir el texto correspondiente
+
+	mov rdi, [current_score]		; Valor actual del puntaje
+    call num_to_str					; Se llama a la funcion para convertir numeros a texto
+
+	add r8, (column_cells+5)*10
+	add r8, 16						; Se ubica la posicion donde se quiere colocar el nivel
+
+	mov rsi, level_text				; Texto 'NIVEL'
+	mov rcx, level_length			; Longitud del texto
+	call write_score
+
+	mov rdi, [current_level]		; Valor del nivel actual
+    call num_to_str
+
+	add r8, (column_cells+5)*15		
+	sub r8, 46						; Se ubica la posicion donde se quieren colocar las vidas
+
+	mov rsi, lives_text				; Texto 'VIDAS'
+	mov rcx, lives_length			; Longitud del texto
+	call write_score
+
+	mov rdi, [current_lives]		; Valor de vidas actuales
+    call num_to_str
+
+	pop r13
+	pop r12
+	pop rdx
+	pop rbx
+	ret
+
+; Funcion para convertir numero a string
+num_to_str:
+	push rbx
+	push rdx
+	mov rbx, 10			; Divisor para obtener los digitos
+	mov rax, rdi        ; Numero a convertir (niveles, vidas o puntos)
+    xor rcx, rcx        ; Contador
+    
+.divide_loop:
+    xor rdx, rdx        ; Limpiar rdx para la division
+    div rbx             ; Dividir por 10
+    push rdx            ; Guardar el residuo en el stack
+    inc rcx             ; Incrementar contador
+    test rax, rax       ; Verificar si quedan digitos
+    jnz .divide_loop
+    
+.write_loop:
+    pop rdx             ; Obtener digitos del stack
+    add dl, '0'         ; Convertir a ASCII
+    mov [r8], dl        ; Escribir digito
+    inc r8              ; Siguiente espacio en memoria
+    dec rcx             ; Decrementar contador
+    jnz .write_loop
+    
+    pop rdx
+    pop rbx
+    ret
+
+; Funcion para escribir texto
+write_score:
+    push rcx
+    push rsi
+    
+.write_loop:
+    mov al, byte [rsi]
+    mov [r8], al
+    inc rsi
+    inc r8
+    dec rcx
+    jnz .write_loop
+    
+    pop rsi
+    pop rcx
+    ret
+
+
+
+_start:
+	call canonical_off
+	print clear, clear_length
+	call start_screen
+
+	; Inicializar posición de la pelota
+    mov qword [ball_pos_x], 15
+    mov qword [ball_pos_y], 15
+    mov qword [ball_dir_x], 1
+    mov qword [ball_dir_y], 1
+    mov qword [last_ball_pos_x], 15
+    mov qword [last_ball_pos_y], 15
+
+
+
+	.main_loop:
+		;print clear, clear_length
+		call move_ball
+		call print_ball
+		call print_pallet
+		call draw_blocks_m
+		call score_info
+		print board, board_size
+		;setnonblocking
+	.read_more:
+		getchar
+
+		cmp rax, 1
+    	jne .done
+
+		mov al,[input_char]
+
+		cmp al, 'a'
+	    jne .not_left
+	    mov rdi, left_direction
+		call move_pallet
+	    jmp .done
+
+		.not_left:
+		 	cmp al, 'd'
+	    	jne .not_right
+			mov rdi, right_direction
+	    	call move_pallet
+    		jmp .done
+
+		.not_right:
+
+    		cmp al, 'q'
+    		je exit
+
+			jmp .read_more
+
+		.done:
+			;unsetnonblocking
+			sleeptime
+			print clear, clear_length
+    		jmp .main_loop
+
+		print clear, clear_length
+
+		jmp exit
+
+
+start_screen:
+
+	call canonical_on
+	print msg1, msg1_length
+	getchar
+	call canonical_off
+	print clear, clear_length
+	ret
+
+exit:
+	call canonical_on
+	mov    rax, 60
+    mov    rdi, 0
+    syscall
